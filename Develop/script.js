1c5d4f9dd172407fb552647472e8a410aabcc62e
Brandon Jackson <brandonljackson23@gmail.com>
2020-05-31 13:03:49 +1000

    save and pull from local storage, time slot styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,75 @@
-var timeArray = {};
-var timeBlockArray = {};
-var list = [];
+// variables for each time slot
+var nine = JSON.parse(localStorage.getItem('nine')) || [];
+var ten = JSON.parse(localStorage.getItem('ten')) || [];
+var eleven = JSON.parse(localStorage.getItem('eleven')) || [];
 
 // display current date in header by appending 'p' element
 $("#currentDay").prepend(moment().format("dddd MMMM, Do"));
 
+// load time slot items from localStorage
+$("#item-nine").append(nine);
+$("#item-ten").append(ten);
+$("#item-ten").append(eleven);
 
-var timeColor = function() {
-    var timeArray = $(".text-col");
-
-    $.each(timeArray, function (i, timeEl) {
-        var hourCheck = $(timeEl).data("hour");
-        var currentHour = moment().hour();
-        console.log(currentHour);
-        $(timeEl).removeClass("past present future")
-
-        if (moment(currentHour).isAfter(hourCheck)) {
-            $(timeEl).addClass("past");
-        } else if (moment(currentHour).isBefore(hourCheck)) {
-            $(timeEl).addClass("future");
-        } else {
-            $(timeEl).addClass("present");
-        }
-    })
+// save time slot items to localStorage, replacing existing values
+$("#save-nine").on("click", function() {
+    var item = $("#item-nine").val().trim();
+    nine.splice(0);
+    nine.push(item);
+    localStorage.setItem("nine", JSON.stringify(nine));
+});
+$("#save-ten").on("click", function() {
+    var item = $("#item-ten").val().trim();
+    ten.splice(0);
+    ten.push(item);
+    localStorage.setItem("ten", JSON.stringify(ten));
+});
+$("#save-eleven").on("click", function() {
+    var item = $("#item-eleven").val().trim();
+    ten.splice(0);
+    ten.push(item);
+    localStorage.setItem("eleven", JSON.stringify(eleven));
+});
+// add styling to time slot based on current time
+function nineStyling() {
+    var timeSlot = moment({hour:9});
+    var currentTime = moment().hour();
+    console.log("timeslot " + timeSlot);
+    console.log("currentTime " + currentTime);
+    if (moment(currentTime).isAfter(timeSlot)) {
+        $("#time-nine").addClass("past");
+    } else if (moment(currentTime).isBefore(timeSlot)) {
+        $("#time-nine").addClass("future");
+    } else {
+        $("#time-nine").addClass("present");
+    }
 };
-
-timeColor();
-
-setInterval(function() {
-    $(".text-col").each(function(tc) {
-        timeColor(tc);
-    });
-}, 3000);
-
-$('#item-nine-am').on('blur', function() {
-    var item = $('#item-nine-am').val().trim();
-    list.push(item);
-    console.log(list);
-    localStorage.setItem('nine-am', JSON.stringify(list));
-})
+nineStyling();
+function tenStyling() {
+    var timeSlot = moment({hour:10});
+    var currentTime = moment().hour();
+    console.log("timeslot " + timeSlot);
+    console.log("currentTime " + currentTime);
+    if (moment(currentTime).isAfter(timeSlot)) {
+        $("#time-ten").addClass("past");
+    } else if (moment(currentTime).isBefore(timeSlot)) {
+        $("#time-ten").addClass("future");
+    } else {
+        $("#time-ten").addClass("present");
+    }
+};
+tenStyling();
+function elevenStyling() {
+    var timeSlot = moment({hour:11});
+    var currentTime = moment().hour();
+    console.log("timeslot " + timeSlot);
+    console.log("currentTime " + currentTime);
+    if (moment(currentTime).isAfter(timeSlot)) {
+        $("#time-eleven").addClass("past");
+    } else if (moment(currentTime).isBefore(timeSlot)) {
+        $("#time-eleven").addClass("future");
+    } else {
+        $("#time-eleven").addClass("present");
+    }
+};
+elevenStyling();
